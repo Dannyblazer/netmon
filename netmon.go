0c0900ad7fa86854ddedf263d83c8f322b8fa637
Dannyblazer <nnajidanny00@gmail.com>
2025-09-28 13:01:38 +0100

    http monitor test

--- netmon.go
+++ netmon.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func checkHTTP(url string) (time.Duration, error) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	return time.Since(start), nil
+}
+
+func monitorHTTP(url string) {
+	for {
+		duration, err := checkHTTP(url)
+		if err != nil {
+			fmt.Printf("HTTP check %s failed: %v\n", url, err)
+		} else {
+			fmt.Printf("HTTP check %s: %v\n", url, duration)
+		}
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func main() {
+	go monitorHTTP("https://www.google.com")
+	select {}
+}
